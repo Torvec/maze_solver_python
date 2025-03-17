ac485fa11ea8e0a07d61c6634b3cacc020bb9143
vonedward <me@edward-vonschondorf.dev>
2025-03-17 13:43:45 -0700

    Modularize project, add solver to maze

---
+++ cell.py
@@ -0,0 +1,58 @@
+from graphics import Line, Point
+
+class Cell:
+    def __init__(self, win=None):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_btm_wall = True
+        self.visited = False
+        self._x1 = None
+        self._y1 = None
+        self._x2 = None
+        self._y2 = None
+        self._win = win
+
+    def draw(self, x1, y1, x2, y2):
+        if self._win is None:
+            return
+        self._x1 = x1
+        self._y1 = y1
+        self._x2 = x2
+        self._y2 = y2
+        if self.has_left_wall:
+            line = Line(Point(x1, y1), Point(x1, y2))
+            self._win.draw_line(line)
+        else:
+            line = Line(Point(x1, y1), Point(x1, y2))
+            self._win.draw_line(line, "white")
+        if self.has_top_wall:
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self._win.draw_line(line)
+        else:
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self._win.draw_line(line, "white")
+        if self.has_right_wall:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self._win.draw_line(line)
+        else:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self._win.draw_line(line, "white")
+        if self.has_btm_wall:
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self._win.draw_line(line)
+        else:
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self._win.draw_line(line, "white")
+        
+
+    def draw_move(self, to_cell, undo=False):
+        origin_center_x = (self._x1 + self._x2) // 2
+        origin_center_y = (self._y1 + self._y2) // 2
+        dest_center_x = (to_cell._x1 + to_cell._x2) // 2
+        dest_center_y = (to_cell._y1 + to_cell._y2) // 2
+        fill_color = "red"
+        if undo:
+            fill_color = "gray"
+        line = Line(Point(origin_center_x, origin_center_y), Point(dest_center_x, dest_center_y))
+        self._win.draw_line(line, fill_color)
